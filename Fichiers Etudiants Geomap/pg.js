0fcfd477033b02297e2c837959fd92933d6f15ee
Roskai <93260994+Roskai@users.noreply.github.com>
2023-01-17 21:31:39 +0100

    Update pg.js

--- Fichiers Etudiants Geomap/pg.js
+++ Fichiers Etudiants Geomap/pg.js
@@ -8,26 +8,53 @@ function initialiserTableau (){
 	tabDpt["Hautes-alpes"]="Paca"
 	tabDpt["Charente"]="Poitou-charentes"
 	tabDpt["Vienne"]="Poitou-charentes"
-	console.log(tabDpt);
+
+	tabPref["Dordogne"]="Perigueux"
+ 	tabPref["Landes"]="Dax"
+ 	tabPref["Var"]="Toulon"
+	tabPref["Vaucluse"]="Avignon"
+  	tabPref["Hautes-alpes"]="Gap"
+  	tabPref["Charente"]="Angouleme"
+	tabPref["Vienne"]="Poitiers"
 
 }
 
 
 function getHTMLZoneDeListe(laRegion){
-  var html = "<select name='dpt' id='dpt'>";
-	var indice;
+	var html = "<select name='dpt' id='dpt' onchange='afficherPref(this.value)'>";
 	initialiserTableau();
 	for (var dpt in tabDpt) {
-	 	if (tabDpt[dpt] === laRegion) {
-		 	html += "<option value='" + dpt + "'>" + dpt + "</option>";
-	 	}
- 	}
-  html += "</select>";
-  return html;
-
+		if (tabDpt[dpt] === laRegion) {
+			html += "<option value='" + dpt + "'>" + dpt + "</option>";
+		}
+	}
+	html += "</select>";
+	html += "<div id='prefecture'></div>";
+	return html;
 }
 function afficherZoneDeListe(){
 	var laRegion;
 	laRegion=document.getElementById("reg").value;
 	document.getElementById("liste").innerHTML=getHTMLZoneDeListe(laRegion);
+}
+/*
+fonction getHTMLImg(leDépartement : chaîne de caractères) : chaïne de caractères
+    dÃ©but
+        codeHTML <- "<img src='img/" + leDépartement + ".jpg'>"
+        renvoyer codeHTML
+    fin
+*/
+function getHTMLImg(leDepartement) {
+   return "<img src='img/" + leDepartement + ".jpg'>";
+}
+
+function afficherImage() {
+  let departement = document.getElementById("dpt").value;
+  let codeHTML = getHTMLImg(departement);
+  document.getElementById("res").innerHTML = codeHTML;
+}
+
+function afficherPref(dpt) {
+  var pref = tabPref[dpt];
+  document.getElementById("prefecture").innerHTML = "Préfecture : " + pref;
 }
